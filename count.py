2c3379ec05166ac9b01ce4c315185c741b081b65
annaeliz <annelizabethleju@gmail.com>
2021-09-06 22:45:20 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,19 +1,31 @@
 from pathlib import Path
 basepath = Path(input("Please enter the Folder name: "))
 def countword(basepath):
-    files_in_basepath = basepath.iterdir()
-    for item in files_in_basepath:
-        if item.is_file():
-            print(item.name)
+    try:
+            files_in_basepath = basepath.iterdir()
+            for item in files_in_basepath:
+                if item.is_file():
+                    print(item.name)
+                    try:
+                        f=open(item,"r")
+                        data=f.read()
+                        data=data.replace(","," ")
+                        word=data.split()
+                        print(item,"-", len(word))
+                    except UnicodeDecodeError:
+                        pass
+                        print("Please enter vaild File/Folder name")
+    except:
+            print("Files")
             try:
-                f=open(item,"r")
-                data=f.read()
-                data=data.replace(","," ")
-                word=data.split()
-                print(item,"-", len(word))
+                        f=open(basepath,"r")
+                        data=f.read()
+                        data=data.replace(","," ")
+                        word=data.split()
+                        print(basepath,"-", len(word))
             except UnicodeDecodeError:
-                pass
-                print("Please enter vaild File/Folder name")
+                        pass
+                        print("Please enter vaild File/Folder name")
     return(basepath)
 print(countword(basepath))   
 
